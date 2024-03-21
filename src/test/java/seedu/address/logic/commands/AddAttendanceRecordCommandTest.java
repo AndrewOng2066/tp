17674359925c3b13b02d0d2d2767e5a4165106d9
AndrewOng2066 <e0959899@u.nus.edu>
2024-03-21 18:45:31 +0800

    Update nothing

--- src/test/java/seedu/address/logic/commands/AddAttendanceRecordCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddAttendanceRecordCommandTest.java
@@ -1,4 +1,17 @@
+package seedu.address.logic.commands;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalPersons.getTypicalClassBook;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
 class AddAttendanceRecordCommandTest {
-  
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs(), getTypicalClassBook());
+
+
+
 }
